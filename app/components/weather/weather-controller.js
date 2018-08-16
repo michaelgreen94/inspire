0811c9914e3e1af372e1d2293d8ec24b465ae38d
Michael Green <michael.green94@hotmail.com>
2018-08-16 17:47:44 -0600

    first init

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -1,6 +1,15 @@
 import WeatherService from "./weather-service.js";
 
 var weatherService = new WeatherService()
+const outside = document.getElementById('weather')
+
+function drawWeather(weather) {
+	let template = `
+	<div>hello</div>
+	<div>from</div>
+	<div>drawWeather</div>`
+	outside.innerHTML = template
+}
 
 
 export default class WeatherController {
